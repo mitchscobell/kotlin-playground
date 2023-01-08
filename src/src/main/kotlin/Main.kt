d76d90a58016f8c3fd9f0a37b913831716b43f2b
Mitch Scobell <mitchscobell@gmail.com>
2023-01-08 00:15:00 -0600

    Adding comment with course info

--- src/src/main/kotlin/Main.kt
+++ src/src/main/kotlin/Main.kt
@@ -5,4 +5,5 @@ fun main(args: Array<String>) {
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 
+    // https://www.codecademy.com/courses/learn-kotlin/lessons/kotlin-data-types-variables/exercises/introduction
 }
