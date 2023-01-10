70df9032788c9709a11de0b158933b587b2ead8f
Mitch Scobell <mitchscobell@gmail.com>
2023-01-10 09:07:23 -0600

    Adding some notes about variables

--- src/src/main/kotlin/Main.kt
+++ src/src/main/kotlin/Main.kt
@@ -6,4 +6,17 @@ fun main(args: Array<String>) {
     println("Program arguments: ${args.joinToString()}")
 
     // https://www.codecademy.com/courses/learn-kotlin/lessons/kotlin-data-types-variables/exercises/introduction
+    mutableVariables();
+    immutableVariables();
+}
+
+fun mutableVariables() {
+    var someDate: String = "01/10/2023";
+    println(someDate);
+}
+
+fun immutableVariables() {
+    // val is used like const
+    val pi: Double = 3.14
+    println(pi);
 }
